8399442e91c25d228586310d2bd157ff514aa0a5
Anastasia Orishchenko <orishchenko@i.ua>
2017-10-17 02:02:59 +0300

    Initial Commit

---
+++ FirstXCodeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstXCodeTest
+//
+//  Created by Anastasia Orishchenko on 10/17/17.
+//  Copyright © 2017 Anastasiia Orishchenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FirstXCodeTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FirstXCodeTest
+//
+//  Created by Anastasia Orishchenko on 10/17/17.
+//  Copyright © 2017 Anastasiia Orishchenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
